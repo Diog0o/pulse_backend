b628a3de706b8ab6f15f477f11d52db46f5808fc
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-02-21 00:45:41 +0000

    Feat: comment Schema

---
+++ models/commentSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    post_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+
+    content: {
+        type: String,
+        trim: true,
+        required: [true, 'Please provide a content']
+    },
+
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+})
