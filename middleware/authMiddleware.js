b6656a8f36335e8ed70ef7280ca334f13fb3eed2
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-02-24 15:55:38 +0000

    Add: authMiddleware for user authentication

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,22 +1,25 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const User = require("../models/userSchema");
 
 dotenv.config();
 
-const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+const authenticateUser = async (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: 'Access denied. No token provided.' });
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Attach the decoded user data to the request object
+    req.user = await User.findById(decoded.userId).select("-password");
     next();
   } catch (error) {
-    res.status(400).json({ message: 'Invalid token.' });
+    res.status(400).json({ message: "Invalid token." });
   }
 };
 
-module.exports = authMiddleware;
+module.exports = authenticateUser;
